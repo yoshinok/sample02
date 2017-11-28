1da9791161fb233d07bd52e778fcc0f92e6865e8
yoshinok <yoshino-k@macnica.net>
2017-11-28 18:10:10 +0900

    changed from gui to cli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 public class Main{
   public static void main(String arg[]){
-    JFrame frame = new JFrame("taro");
+    /*JFrame frame = new JFrame("taro");
     frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    */
+    int i = 100;
+    System.out.println(100);
   }
 }
