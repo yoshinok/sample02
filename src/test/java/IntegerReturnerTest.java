881ae6d79d009702de94972730f67dac0dba2438
yoshinok <yoshino-k@macnica.net>
2017-11-29 11:20:15 +0900

    changed to fail automatically

--- src/test/java/IntegerReturnerTest.java
+++ src/test/java/IntegerReturnerTest.java
@@ -7,7 +7,7 @@ public class IntegerReturnerTest{
   @Test
   public void testA(){
     //assertEquals(1,IntegerReturner.testee());
-    //fail();
-    assertTrue(true);
+    fail();
+    //assertTrue(true);
   }
 }
